51d6706a48a842d38b11f5558f4dfc75219214d3
deep-webdev <deeppanchal2410@gmail.com>
2022-04-23 11:03:56 +0530

    [ADD] New and First Initial Commit for Rest Framework

---
+++ MyProject/rest_api/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from . views import article_detail, article_list, article_list_view, article_detail_view, ArticleAPIView, ArticleDetails
+
+urlpatterns = [
+    # For Function Based View
+    path('article/', article_list),
+    path('details/<int:pk>', article_detail),
+    path('browse_article/', article_list_view),
+    path('browse_article_detail/<int:pk>', article_detail_view),
+    
+    # For Class Based View
+    path('', ArticleAPIView.as_view()),
+    path('detail_class/<int:id>', ArticleDetails.as_view())
+    
+]
